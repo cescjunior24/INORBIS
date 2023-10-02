46b6c6729cae7c787da51e2de83d08809ef1e03f
cescjunior24 <91435675+cescjunior24@users.noreply.github.com>
2023-10-02 15:25:55 +0200

    Pujada Exercici 4

---
+++ Ex4.py
@@ -0,0 +1,28 @@
+
+
+from array import array
+
+
+def ImparellParell(num): #imparell=false #parell=true
+    return false
+
+
+
+
+
+def Corrector():
+    array_numeros=[22,33,44,50,100,55,74,73,2]
+    array_resultat=[True,False,True,True,True,False,True,False,True]
+    array_resultat_alumne=[]
+    for numero in array_numeros:
+        array_resultat_alumne.append(ImparellParell(array_numeros))
+    
+    if(array_resultat==array_resultat_alumne):
+        print("TEST OK")
+    else:
+        print("TEST NOT OK")
+
+        
+        
+
+Corrector()
